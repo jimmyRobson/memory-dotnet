08ec02cceadf4884a383c2863afa0234dd03cbb8
Jimmy Robson <jrobson3@alaska.edu>
2017-08-17 00:14:30 -0800

    Remove sql files

--- queries.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER webappuser
-    FOR LOGIN webappuser
-GO
-
-EXEC sp_addrolemember 'db_datareader', 'webappuser'
-EXEC sp_addrolemember 'db_datawriter', 'webappuser'
-EXEC sp_addrolemember 'db_ddladmin', 'webappuser'
-GO
